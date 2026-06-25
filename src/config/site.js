33714572522b609943b7b97a035acf54d3e9d366
路过的人 <18276322632@163.com>
2026-06-25 19:38:51 +0800

    Update site.js

--- src/config/site.js
+++ src/config/site.js
@@ -2,10 +2,11 @@ export const siteConfig = {
   name: 'ca(oH)2',
   subtitle: 'Personal Journal',
   githubUrl: 'https://github.com/',
+
   background: {
-    image: `url("${process.env.BASE_URL}images/backgrounds/study-desk.svg")`,
-    opacity: 0.65,
-    overlayOpacity: 0.3,
+    image: 'url("https://broli-ux.github.io/qingyanghuagai/images/backgrounds/study-desk.svg")',
+    opacity: 0.75,
+    overlayOpacity: 0.2,
     blur: 0,
     position: 'center center',
     size: 'cover',
