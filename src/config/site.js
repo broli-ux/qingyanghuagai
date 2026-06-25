52d90bc54652dd53d17be4eefcc0243f59b80d56
路过的人 <18276322632@163.com>
2026-06-25 19:12:15 +0800

    Update site.js

--- src/config/site.js
+++ src/config/site.js
@@ -1,12 +1,11 @@
-
 export const siteConfig = {
   name: 'ca(oH)2',
   subtitle: 'Personal Journal',
   githubUrl: 'https://github.com/',
   background: {
-    image: 'url("images/backgrounds/study-desk.svg")',
-    opacity: 0.34,
-    overlayOpacity: 0.72,
+    image: `url("${process.env.BASE_URL}images/backgrounds/study-desk.svg")`,
+    opacity: 0.65,
+    overlayOpacity: 0.3,
     blur: 0,
     position: 'center center',
     size: 'cover',
